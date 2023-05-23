a8a7b7e77aaecf2c8c01e8920a6a0ba8a76ee8f3
jithstephen13 <jithstephen96@gmail.com>
2023-05-23 19:18:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,345 @@
+let Desks=[
+    {name:"large straight",wt:90,t:"",twt:0},
+    {name:"Sm Straight",wt:55,t:"",twt:0},
+    {name:"L 1.3",wt:65,t:"",twt:0},
+    {name:"L 1.8",wt:90,t:"",twt:0},
+    {name:"120 degree",wt:65,t:"",twt:0},
+    {name:"Desk – reception",wt:250,t:"",twt:0},
+    {name:"Desk risers",wt:25,t:"",twt:0},
+   ]
+
+
+   let Storage1=[
+ { name:" Bookcase – Lg",wt:	80,	t:"",twt:	0},
+ { name:"Bookcase – Small",wt:	40,	t:"",twt:	0},
+ { name:"Credenza - Lg",wt:	100,	t:"",twt:	0},
+ { name:"Credenza – Small",wt:	60,	t:"",twt:	0},
+ { name:"Cubby unit",wt:	50,	t:"",twt:	0},
+ { name:"4 Drawer Filing Cabinet",wt:	50,	t:"",twt:	0},	
+ { name:"3 Drawer Filing Cabinet",wt:	40,	t:"",twt:	0},	
+ { name:"2 Drawer Filing Cabinet",wt:	30,	t:"",twt:	0},
+ { name:"Hutch",wt:	30,	t:"",twt:	0},
+ { name:"Lockers per bay ",wt:	25,	t:"",twt:	0},
+
+ { name:"Lundia per bay",wt:	30,	t:"",twt:	0},
+ { name:"Mobiles - Double",wt:	45,	t:"",twt:	0},
+ { name:"Mobiles - Single",wt:	30,	t:"",twt:	0},
+
+ { name:"Server Racking",wt:	60,	t:"",twt:	0},
+
+ { name:"Small Cupboard",wt:	50,	t:"",twt:	0},	
+
+ { name:"Storage cab",wt:	100,	t:"",twt:	0},	
+ { name:"Tambour – Lg",wt:	80,	t:"",twt:	0},
+ { name:"Tambour – small",wt:	40,	t:"",twt:	0},
+ { name:"Tambour - med ",wt:	60,	t:"",twt:	0},
+ { name:"Wall mounted open shelfs ",wt:	50,	t:"",twt:	0},
+   ]
+
+ let Chairs=[
+
+ { name:" Boardroom",wt:	30,	t:"",twt:	0},
+ { name:"Task",wt:	21,	t:"",twt:	0},
+ { name:"Poof",wt:	10,	t:"",twt:	0},
+ { name:"Reception",wt:	20,	t:"",twt:	0},
+ { name:"Stacker",wt:	15,	t:"",twt:	0},
+ { name:"Couch",wt:	100,	t:"",twt:	0},	
+ { name:"Lounge Chair",wt:	35,	t:"",twt:	0},	
+ ]  
+
+
+ 
+ let Tables=[
+
+ { name:" Coffee Table",wt:	20,	t:"",twt:	0},
+ { name:"Round table",wt:	60,	t:"",twt:	0},
+ { name:"Sm Table",wt:	50,	t:"",twt:	0},
+ { name:"Table - BBQ",wt:	80,	t:"",twt:	0},
+ { name:"Table – Boardroom lg",wt:	150,	t:"",twt:	0},
+ { name:"Table – Boardroom sm",wt:	80,	t:"",twt:	0},	
+ { name:"Table - Cafe",wt:	50,	t:"",twt:	0},	
+
+
+ { name:"Table Lg",wt:	80,	t:"",twt:	0},
+ { name:"Table – Folding",wt:	30,	t:"",twt:	0},
+ { name:"Table – Trolley",wt:	40,	t:"",twt:	0},
+  
+ ]  
+ let Partitioning=[ { name:"Partition shelfs",wt:	3,	t:"",twt:	0},
+              { name:"Dividers",wt:	10,	t:"",twt:	0},
+              { name:"Partitions",wt:	20,	t:"",twt:	0},]
+
+                
+   let Boards = [
+         { name:"Boards - Cork",wt:	5,	t:"",twt:	0},
+         { name:"Boards - Pin",wt:	5,	t:"",twt:	0},
+          { name:"Boards - White",wt:	10,	t:"",twt:	0},
+         { name:"Boards - White on wheels",wt:	15,	t:"",twt:	0},
+          { name:"Elec whiteboard",wt:	50,	t:"",twt:	0},
+   ]
+   let Kitchen= [
+         { name:"Bar fridge",wt:	50,	t:"",twt:	0},
+         { name:"Bar leaners",wt:	20,	t:"",twt:	0},
+          { name:"Bar Stool",wt:	20,	t:"",twt:	0},
+         { name:"Dishwasher",wt:	50,	t:"",twt:	0},
+          { name:"Fridge	",wt:	80,	t:"",twt:	0},
+         { name:"Kitchen items per box",wt:10,   t:"",twt:	0},
+         { name:"Microvwae",wt:	15,	t:"",twt:	0},
+       ]
+
+   let Tech=
+         [
+         { name:"Fan",wt:10,	t:"",twt:	0},
+         { name:"Heater",wt:10,	t:"",twt:	0},
+          { name:"Printer",wt:	10,	t:"",twt:	0},
+         { name:"Shredder",wt:	30,	t:"",twt:	0},
+          { name:"TV",wt:	10,	t:"",twt:	0},
+   ]
+
+   let Other=[
+   { name:"Art work",wt:	10,	t:"",twt:	0},
+ { name:"BBQ",wt:	50,	t:1,twt:	50},
+ { name:"Bin",wt:	2,	t:"",twt:	0},
+ { name:"Carpet Rug",wt:30,	t:"",twt:	0},
+ { name:"Carpet Tiles",wt:	1,	t:"",twt:	0},	
+
+ { name:"Foot stools",wt:	10,	t:"",twt:	0},	
+ { name:"Hat stand",wt:	15,	t:"",twt:	0},
+ { name:"HutOffice stationary (Box)",wt:	10,	t:"",twt:	0},
+ { name:"Plants ",wt:	20,	t:"",twt:	0},
+ { name:"Single Mattress",wt:	17,	t:"1",twt:	17},
+         { name:"Single Base",wt:	25,	t:"1	",twt:	25},
+          { name:"Double Mattress",wt:	21,	t:"1",twt:	21},
+         { name:"DiDouble Base ",wt:	38,	t:"11",twt:	418},
+          { name:"Queen Mattress",wt:	30,	t:"1",twt:	30},
+      { name:"KQueen Base",wt:44,   t:"1",twt:	44},
+     { name:"King Mattress",wt:	72,	t:"1",twt:	072},
+         { name:"King Base",wt:	46,	t:"1",twt:	46},
+         { name:"Other 2",wt:"",	t:"0",twt:0	},
+         { name:"Other 3",wt:"",	t:"0",twt:0	},
+         { name:"Other 4",wt:"",	t:"0",twt:	0},
+         { name:"Other 5",wt:"",	t:"0",twt:	0},
+
+
+   ]
+
+
+   const Finel={name:"WD Fee/1000kgs:",wt:"240",t:"Total WD Fee :", }
+   
+
+
+
+
+
+
+
+   
+   let container1=document.getElementById("table_container1")
+   let container2=document.getElementById("table_container2")
+   let container3=document.getElementById("table_container3")
+   let container4=document.getElementById("table_container4")
+   let container5=document.getElementById("table_container5")
+   let container6=document.getElementById("table_container6")
+   let container7=document.getElementById("table_container7")
+   let container8=document.getElementById("table_container8")
+   let container9=document.getElementById("table_container9")
+   let container10=document.getElementById("table_container10")
+   let container11=document.getElementById("table_container11")
+   let container12=document.getElementById("table_container12")
+   
+ 
+
+   function Display (data,text,container){
+    container.innerHTML="" 
+    let head=document.createElement("thead")
+    head.innerText=text
+    head.style.backgroundColor="pink"
+    container.append(head)
+    data.forEach((el,idx)=>{
+       
+
+      let row= document.createElement("tr")
+      let col1= document.createElement("td")
+        col1.innerText=el.name
+      let col2= document.createElement("td")
+      let inp2=document.createElement("input") 
+      inp2.value=el.wt
+
+      inp2.addEventListener("change",(e)=>{
+        
+        let val=e.target.value
+        
+         Changes(data,val,idx,"wt")
+      })
+      col2.append(inp2)
+      let col3= document.createElement("td")
+      let inp3=document.createElement("input") 
+      inp3.value=el.t ? el.t:""
+      inp3.addEventListener("change",(e)=>{
+        let val=e.target.value
+        
+        Changes(data,val,idx,"t")
+      })
+      col3.append(inp3)
+
+       let col4= document.createElement("td")
+        col4.innerText=el.wt*(el.t ?el.t :0)
+
+        row.append(col1,col2,col3,col4)
+        container.append(row)
+    })
+
+
+   }
+   Display (Desks,"Desk",container1)
+   Display (Storage1,"Storage",container2)
+   Display (Chairs,"Chairs",container3)
+   Display (Tables,"Tables",container4)
+   Display (Partitioning,"Partitioning",container5)
+   Display (Boards,"Boards",container6)
+   Display (Kitchen,"Kitchen",container7)
+   Display (Tech,"Tech",container8)
+   Display (Other,"Other",container9)
+    
+   
+
+
+
+function Changes(data,val,idx,col){
+
+console.log(val, col)
+if(col=="wt"){
+data[idx].wt=val
+data[idx].twt=data[idx].wt*data[idx].t
+
+}
+else{
+data[idx].t=val
+data[idx].twt=data[idx].wt*data[idx].t
+}
+
+   Display (Desks,"Desk",container1)
+   Display (Storage1,"Storage",container2)
+   Display (Chairs,"Chairs",container3)
+   Display (Tables,"Tables",container4)
+   Display (Partitioning,"Partitioning",container5)
+   Display (Boards,"Boards",container6)
+   Display (Kitchen,"Kitchen",container7)
+   Display (Tech,"Tech",container8)
+   Display (Other,"Other",container9)
+   Total()
+   Subtotal()
+ 
+}
+
+
+
+let Tot=0
+function Total(){
+
+let t=0
+Desks.forEach((el)=>{
+    t+=Number(el.t)
+    Tot+=el.twt
+})
+Storage1.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+Tables.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+Partitioning.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+Boards.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+Kitchen.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+Tech.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+Other.forEach((el)=>{
+t+=Number(el.t)
+Tot+=el.twt
+})
+
+  
+let container=document.getElementById("table_total")
+container.innerHTML=""
+
+     let row= document.createElement("tr")
+     row.style.backgroundColor="#f0f1f7"
+      let col1= document.createElement("td")
+      col1.innerText="Total :"
+      let col2= document.createElement("td")
+      let col3= document.createElement("td")
+      col3.innerText=t
+      let col4= document.createElement("td")
+      col4.innerText=Tot
+
+
+
+      
+
+      row.append(col1,col2,col3,col4)
+      container.append(row)
+
+
+}
+Total()   
+
+
+function Subtotal(){
+
+
+let container=document.getElementById("table_Subtotal")
+container.innerHTML=""
+
+     let row= document.createElement("tr")
+     row.style.backgroundColor="#f0f157"
+      let col1= document.createElement("td")
+      col1.innerText=Finel.name
+      let col2= document.createElement("td")
+      let inp2=document.createElement("input") 
+      inp2.value=`$${Finel.wt}`
+
+      inp2.addEventListener("change",(e)=>{
+        
+        let val=e.target.value
+        
+        Finel.wt=val
+        Subtotal()
+      })
+      col2.append(inp2)
+      let col3= document.createElement("td")
+      let inp3=document.createElement("input") 
+      col3.innerText=Finel.t ? Finel.t:""
+      inp3.addEventListener("change",(e)=>{
+        let val=e.target.value
+        
+        Changes(data,val,idx,"t")
+      })
+     
+
+       let col4= document.createElement("td")
+        col4.innerText=Tot/1000*Number(Finel.wt)
+
+
+
+
+      
+
+      row.append(col1,col2,col3,col4)
+      container.append(row)
+
+
+}
+Subtotal()
